bdb672dcc3242fa97f98ed0cc243d0bd3643c42f
franki <frankiindrawan@gmail.com>
2021-07-31 16:35:23 +0800

    add product data

--- BkpGasProcurementSystem/Models/Product.cs
+++ BkpGasProcurementSystem/Models/Product.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BkpGasProcurementSystem.Models
 {
     public class Product
     {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        public decimal Weight { get; set; }
+
     }
 }
